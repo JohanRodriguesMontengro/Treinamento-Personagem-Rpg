fd3f83b35e1b7f56c225a2168cd8f7f265b6d93f
Johan <ti.johan@ti7.com.br>
2022-10-25 14:50:49 -0300

    1 commit

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ do {
   let Elemento = ``
   let Defesa = 0
   let NivelPersonagem = 0
-  let Resetar = 0
+  let Reset = 0
   while (EscolherElemento === true) {
     Elemento = prompt(`Escolha seu elemento
      1- Fogo
@@ -30,31 +30,46 @@ do {
   2- Elemento
   3- Status
   4- Sair
-  5- Resetar
+  5- Reset
   Nome: ${NomePersonagem}
   Elemento: ${Elemento}
   Nível: ${NivelPersonagem}
-  Resets: ${Resetar}`)
+  Resets: ${Reset}`)
   PainelDeControle = parseInt(PainelDeControle)
 
     if (isNaN(PainelDeControle) || PainelDeControle <= 0 || PainelDeControle >= 6) {
       alert(`Digite somente números entre 1 a 5`)
     }
     else if (PainelDeControle === 1) {
-      PainelDeTreinamento = prompt(`
-      1- Academia
-      2- 
-      3- 
-      4- 
-      5- 
-      6- Monte Everest
-      7- 
-      8- Estação Espacial Internacional
-      9- Vácuo Do Espaço
-      10- 
-      11- 
-      12- Ton 618
-      13- Sobre os locais`)
+      let Repetidor = true
+      let PainelDeTreinamento = ``
+      do {
+        PainelDeTreinamento = prompt(`
+        1- Academia (Nivel Requirido: 0)
+        2- Cachoeira (Nivel Requirido: 15)
+        3- Castelo Antigo (Nivel Requirido: 60)
+        4- Fukushima (Nivel Requirido: 500)
+        5- Fossa Das Marianas (Nivel Requirido: 4.000)
+        6- Monte Everest (Nivel Requirido: 8.849)
+        7- Krakatoa (Nivel Requirido: )
+        8- Orbita Da Terra (Nivel Requirido:)
+        9- Estação Espacial Internacional (Nivel Requirido:)
+        10- Vácuo Do Espaço (Nivel Requirido:)
+        11- Lua (Nivel Requirido:)
+        12- Sol (Nivel Requirido:)
+        13- Fora Do Sistema Solar (Nivel Requirido:)
+        14- Fora Da Via Láctea (Nivel Requirido:)
+        15- Sobre os locais (Nivel Requirido:)
+        Nivel Atual: ${NivelPersonagem}`)
+  
+        if (isNaN(PainelDeTreinamento) || PainelDeTreinamento >= 16 || PainelDeTreinamento <= 0) {
+          alert(`Digite somente números entre 1 a 16`)
+        }
+        else if (PainelDeTreinamento === 1) {
+
+        }
+      }while(Repetidor === true)
+      
     }
   }
 }while(Continuar === true)
