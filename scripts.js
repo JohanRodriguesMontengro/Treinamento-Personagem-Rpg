03e28229165da2d846a2c8241f9f703a26196af7
Johan <ti.johan@ti7.com.br>
2022-10-25 15:24:54 -0300

    2 commit

--- scripts.js
+++ scripts.js
@@ -46,27 +46,49 @@ do {
       do {
         PainelDeTreinamento = prompt(`
         1- Academia (Nivel Requirido: 0)
-        2- Cachoeira (Nivel Requirido: 15)
-        3- Castelo Antigo (Nivel Requirido: 60)
-        4- Fukushima (Nivel Requirido: 500)
-        5- Fossa Das Marianas (Nivel Requirido: 4.000)
+        2- Cachoeira Do Sul (Nivel Requirido: 15)
+        3- Neuschwanstein (Nivel Requirido: 163)
+        4- Fukushima (Nivel Requirido: 534)
+        5- Fossa Das Marianas (Nivel Requirido: 4.592)
         6- Monte Everest (Nivel Requirido: 8.849)
-        7- Krakatoa (Nivel Requirido: )
-        8- Orbita Da Terra (Nivel Requirido:)
-        9- Estação Espacial Internacional (Nivel Requirido:)
-        10- Vácuo Do Espaço (Nivel Requirido:)
-        11- Lua (Nivel Requirido:)
-        12- Sol (Nivel Requirido:)
-        13- Fora Do Sistema Solar (Nivel Requirido:)
-        14- Fora Da Via Láctea (Nivel Requirido:)
-        15- Sobre os locais (Nivel Requirido:)
+        7- Krakatoa (Nivel Requirido: 15.867)
+        8- Orbita Da Terra (Nivel Requirido: 35.795)
+        9- Estação Espacial Internacional (Nivel Requirido: 78.583)
+        10- Vácuo Do Espaço (Nivel Requirido: 278.945)
+        11- Lua Terrestre(Nivel Requirido: 608.842)
+        12- Sol (Nivel Requirido: 1.598.378)
+        13- Fora Do Sistema Solar (Nivel Requirido: 14.759.645)
+        14- Fora Da Via Láctea (Nivel Requirido: 329.574.297)
+        15- Sobre os locais
         Nivel Atual: ${NivelPersonagem}`)
   
         if (isNaN(PainelDeTreinamento) || PainelDeTreinamento >= 16 || PainelDeTreinamento <= 0) {
-          alert(`Digite somente números entre 1 a 16`)
+          alert(`Digite somente números entre 1 a 15`)
         }
         else if (PainelDeTreinamento === 1) {
-
+          alert(`Você ficou treinando na academia durante 5 horas
+          Você evoluiu 1 nível`)
+          NivelPersonagem += 1
+        }
+        else if (PainelDeTreinamento === 2) {
+          alert(`Você ficou treinando na cachoeira do sul durante 6 horas
+          Você evolui 6 níveis`)
+          NivelPersonagem += 6
+        }
+        else if (PainelDeTreinamento === 3) {
+          alert(`Você ficou treinando na cahoeira do sul durante 6 horas
+          Você evolui 6 níveis`)
+          NivelPersonagem += 6
+        }
+        else if (PainelDeTreinamento === 4) {
+          alert(`Você ficou treinando na cahoeira do sul durante 6 horas
+          Você evolui 6 níveis`)
+          NivelPersonagem += 6
+        }
+        else if (PainelDeTreinamento === 5) {
+          alert(`Você ficou treinando na cahoeira do sul durante 6 horas
+          Você evolui 6 níveis`)
+          NivelPersonagem += 6
         }
       }while(Repetidor === true)
       
